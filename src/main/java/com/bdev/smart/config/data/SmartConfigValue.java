fa058d8e2bc26b5851479c0cc35eb0c5ae79473e
Sergey <serbudnik@gmail.com>
2017-10-29 19:53:37 +0300

    version bump to 1.0.6 safe read / override

--- src/main/java/com/bdev/smart/config/data/SmartConfigValue.java
+++ src/main/java/com/bdev/smart/config/data/SmartConfigValue.java
@@ -8,15 +8,32 @@ import java.util.function.Consumer;
 public class SmartConfigValue<T> {
     private final String name;
     private final AtomicReference<T> value;
+    private final boolean readProtected;
+    private final boolean overrideProtected;
 
     private final CopyOnWriteArrayList<Consumer<T>> listeners = new CopyOnWriteArrayList<>();
 
-    public SmartConfigValue(String name, T value) {
+    public SmartConfigValue(String name, T value, boolean readProtected, boolean overrideProtected) {
         this.name = name;
         this.value = new AtomicReference<>(value);
+
+        this.readProtected = readProtected;
+        this.overrideProtected = overrideProtected;
     }
 
     public T getValue() {
+        return getValue(false);
+    }
+
+    public T forceGetValue() {
+        return getValue(true);
+    }
+
+    private T getValue(boolean forceRead) {
+        if (readProtected && !forceRead) {
+            throw new RuntimeException("Unforced read is forbidden");
+        }
+
         return value.get();
     }
 
@@ -25,6 +42,18 @@ public class SmartConfigValue<T> {
     }
 
     public void override(T newValue) {
+        override(newValue, false);
+    }
+
+    public void forceOverride(T newValue) {
+        override(newValue, true);
+    }
+
+    private void override(T newValue, boolean forceOverride) {
+        if (overrideProtected && !forceOverride) {
+            throw new RuntimeException("Unforced override is forbidden");
+        }
+
         T processedNewValue = SmartConfigValueTypeChecker.process(value.get(), newValue);
 
         value.set(processedNewValue);
